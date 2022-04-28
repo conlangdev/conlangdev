2d059895c0ff1cfeff8d01ec923e7f0f75fd03e0
Marzena <marzena.budek@outlook.com>
2022-04-28 01:21:44 +0100

    📙 Add get and find methods for languages

--- language.go
+++ language.go
@@ -20,10 +20,17 @@ type LanguageUpdate struct {
 	Endonym string `json:"endonym"`
 }
 
+type LanguageCreate struct {
+	Name    string `json:"name" validate:"required"`
+	Slug    string `json:"slug" validate:"required"`
+	Endonym string `json:"endonym"`
+}
+
 type LanguageService interface {
 	GetLanguageByID(ctx context.Context, id uint) (*Language, error)
 	GetLanguageByUserAndSlug(ctx context.Context, user *User, slug string) (*Language, error)
-	CreateLanguage(ctx context.Context, language *Language) error
+	FindLanguagesForUser(ctx context.Context, user *User) ([]*Language, error)
+	CreateLanguageForUser(ctx context.Context, user *User, create LanguageCreate) (*Language, error)
 	UpdateLanguage(ctx context.Context, language *Language, update LanguageUpdate) error
 	DeleteLanguage(ctx context.Context, language *Language) error
 }
--- sql/language.go
+++ sql/language.go
@@ -2,7 +2,8 @@ package sql
 
 import (
 	"context"
-	"errors"
+	"database/sql"
+	"net/http"
 
 	"github.com/conlangdev/conlangdev"
 	"github.com/go-playground/validator/v10"
@@ -18,21 +19,124 @@ func NewLanguageService(db *DB, validate *validator.Validate) *LanguageService {
 }
 
 func (s *LanguageService) GetLanguageByID(ctx context.Context, id uint) (*conlangdev.Language, error) {
-	return nil, errors.New("not implemented")
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	var language conlangdev.Language
+	if err := tx.QueryRowContext(ctx,
+		`SELECT
+			id, created_at, updated_at, name,
+			slug, endonym, user_id
+		FROM languages WHERE id = ? LIMIT 1`,
+		id,
+	).Scan(
+		&language.ID, &language.CreatedAt, &language.UpdatedAt,
+		&language.Name, &language.Slug, &language.Endonym, &language.UserID,
+	); err == sql.ErrNoRows {
+		return nil, &conlangdev.Error{
+			Code:       conlangdev.ENOTFOUND,
+			Message:    "could not find that language",
+			StatusCode: http.StatusNotFound,
+		}
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &language, nil
 }
 
 func (s *LanguageService) GetLanguageByUserAndSlug(ctx context.Context, user *conlangdev.User, slug string) (*conlangdev.Language, error) {
-	return nil, errors.New("not implemented")
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	var language conlangdev.Language
+	if err := tx.QueryRowContext(ctx,
+		`SELECT
+			id, created_at, updated_at, name,
+			slug, endonym, user_id
+		FROM languages WHERE
+			slug = ? AND user_id = ?
+		LIMIT 1`,
+		slug, user.ID,
+	).Scan(
+		&language.ID, &language.CreatedAt, &language.UpdatedAt,
+		&language.Name, &language.Slug, &language.Endonym, &language.UserID,
+	); err == sql.ErrNoRows {
+		return nil, &conlangdev.Error{
+			Code:       conlangdev.ENOTFOUND,
+			Message:    "could not find that language",
+			StatusCode: http.StatusNotFound,
+		}
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &language, nil
 }
 
-func (s *LanguageService) CreateLanguage(ctx context.Context, language *conlangdev.Language) error {
-	return errors.New("not implemented")
+func (s *LanguageService) FindLanguagesForUser(ctx context.Context, user *conlangdev.User) ([]*conlangdev.Language, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	rows, err := tx.QueryContext(ctx,
+		`SELECT
+			id, created_at, updated_at, name,
+			slug, endonym, user_id
+		FROM languages WHERE user_id = ?`,
+		user.ID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	languages := make([]*conlangdev.Language, 0)
+	for rows.Next() {
+		var language conlangdev.Language
+		if err := rows.Scan(
+			&language.ID, &language.CreatedAt, &language.UpdatedAt,
+			&language.Name, &language.Slug, &language.Endonym, &language.UserID,
+		); err != nil {
+			return nil, err
+		}
+		languages = append(languages, &language)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return languages, nil
+}
+
+func (s *LanguageService) CreateLanguageForUser(ctx context.Context, user *conlangdev.User, create conlangdev.LanguageCreate) (*conlangdev.Language, error) {
+	return nil, &conlangdev.Error{
+		Code:       conlangdev.ENOTIMPLEMENTED,
+		Message:    "not implemented",
+		StatusCode: http.StatusInternalServerError,
+	}
 }
 
 func (s *LanguageService) UpdateLanguage(ctx context.Context, language *conlangdev.Language, update conlangdev.LanguageUpdate) error {
-	return errors.New("not implemented")
+	return &conlangdev.Error{
+		Code:       conlangdev.ENOTIMPLEMENTED,
+		Message:    "not implemented",
+		StatusCode: http.StatusInternalServerError,
+	}
 }
 
 func (s *LanguageService) DeleteLanguage(ctx context.Context, language *conlangdev.Language) error {
-	return errors.New("not implemented")
+	return &conlangdev.Error{
+		Code:       conlangdev.ENOTIMPLEMENTED,
+		Message:    "not implemented",
+		StatusCode: http.StatusInternalServerError,
+	}
 }
