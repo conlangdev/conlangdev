5838b642def919f4b42f61f992dda809225e73a2
Marzena <marzena.budek@outlook.com>
2022-04-29 19:46:38 +0100

    📜 Update word schema

--- sql/migrations/0003_words.sql
+++ sql/migrations/0003_words.sql
@@ -1,13 +1,18 @@
 CREATE TABLE words (
     id INT NOT NULL AUTO_INCREMENT,
+    uid INT NOT NULL DEFAULT (UUID_SHORT()),
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     headword VARCHAR(255) NOT NULL,
+    part_of_speech VARCHAR(255) NOT NULL,
     definition TEXT NOT NULL,
+    pronunciation VARCHAR(255),
+    grammar_class VARCHAR(255),
+    gender VARCHAR(255),
     etymology TEXT,
     notes TEXT,
     language_id INT NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT uc_language_slug UNIQUE(slug, language_id),
+    CONSTRAINT uc_uid UNIQUE(uid),
     FOREIGN KEY (language_id) REFERENCES languages(id) ON DELETE CASCADE
 );
--- word.go
+++ word.go
@@ -6,34 +6,48 @@ import (
 )
 
 type Word struct {
-	ID         uint      `json:"id"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
-	Headword   string    `json:"headword"`
-	Definition string    `json:"definition"`
-	Etymology  string    `json:"etymology"`
-	Notes      string    `json:"notes"`
-	LanguageID uint      `json:"language_id"`
+	ID            uint      `json:"id"`
+	UID           uint      `json:"uid"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	Headword      string    `json:"headword"`
+	PartOfSpeech  string    `json:"part_of_speech"`
+	Definition    string    `json:"definition"`
+	Pronunciation string    `json:"pronunciation"`
+	GrammarClass  string    `json:"grammar_class"`
+	Gender        string    `json:"gender"`
+	Etymology     string    `json:"etymology"`
+	Notes         string    `json:"notes"`
+	LanguageID    uint      `json:"language_id"`
 }
 
 type WordUpdate struct {
-	Headword   string `json:"headword"`
-	Definition string `json:"definition"`
-	Etymology  string `json:"etymology"`
-	Notes      string `json:"notes"`
+	Headword      string `json:"headword"`
+	PartOfSpeech  string `json:"part_of_speech"`
+	Definition    string `json:"definition"`
+	Pronunciation string `json:"pronunciation"`
+	GrammarClass  string `json:"grammar_class"`
+	Gender        string `json:"gender"`
+	Etymology     string `json:"etymology"`
+	Notes         string `json:"notes"`
 }
 
 type WordCreate struct {
-	Headword   string `json:"headword"`
-	Definition string `json:"definition"`
-	Etymology  string `json:"etymology"`
-	Notes      string `json:"notes"`
+	Headword      string `json:"headword" validate:"required"`
+	PartOfSpeech  string `json:"part_of_speech" validate:"required"`
+	Definition    string `json:"definition" validate:"required"`
+	Pronunciation string `json:"pronunciation"`
+	GrammarClass  string `json:"grammar_class"`
+	Gender        string `json:"gender"`
+	Etymology     string `json:"etymology"`
+	Notes         string `json:"notes"`
 }
 
 type WordService interface {
-	GetWordByID(ctx context.Context, int uint) (*Word, error)
-	GetWordByLanguageAndSlug(ctx context.Context, language *Language, slug string) (*Word, error)
-	CreateWord(ctx context.Context, word *Word) error
+	GetWordByID(ctx context.Context, id uint) (*Word, error)
+	GetWordByLanguageAndUID(ctx context.Context, language *Language, uid uint) (*Word, error)
+	FindWordsForLanguage(ctx context.Context, language *Language) ([]*Word, error)
+	CreateWord(ctx context.Context, create WordCreate) (*Word, error)
 	UpdateWord(ctx context.Context, word *Word, update WordUpdate) error
 	DeleteWord(ctx context.Context, word *Word) error
 }
